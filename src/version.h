86aa698497e5e3362f10699c942cb97c3535bb21
iMrPoopybutthole <poopybuttholeofficial@gmail.com>
2018-03-04 03:32:09 +0300

    initial commit

---
+++ src/version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern const char* const FLURBO_VERSION_TAG;
+extern const char* const FLURBO_VERSION;
+extern const char* const FLURBO_RELEASE_NAME;
+extern const char* const FLURBO_VERSION_FULL;
