31f02095a6a9a6fe5754119af82d352761e1fe8f
iMrPoopybutthole <poopybuttholeofficial@gmail.com>
2018-03-09 20:53:16 +0300

    Add WHM difficulty adjustment algorithm

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -1,12 +1,12 @@
 // Get last updates on http://flurbo.cc
 //             ___ _  _ ____
-//              |  |--| |===                      
+//              |  |--| |===
 //  _____ __    _____ _____ _____ _____
 // |   __|  |  |  |  | __  | __  |     |
 // |   __|  |__|  |  |    -| __ -|  |  |
 // |__|  |_____|_____|__|__|_____|_____|
 //   ___  ____ ____    _ ____ ____ ___
-//   |--' |--< [__] ___| |=== |___  | 
+//   |--' |--< [__] ___| |=== |___  |
 //
 // Based on "The Monero Project" and "Cryptonote Protocol".
 //
@@ -58,9 +58,12 @@
 #define CURRENT_BLOCK_MAJOR_VERSION                     1
 #define CURRENT_BLOCK_MINOR_VERSION                     0
 #define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT              60*60*2
+#define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V7           60*20
+
 #define CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE             10
 
 #define BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW               60
+#define BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V7            12
 
 // MONEY_SUPPLY - total number coins to be generated
 #define MONEY_SUPPLY                                    (UINT64_C(9999999999900000000))
@@ -92,6 +95,8 @@
 #define DIFFICULTY_CUT                                  60  // timestamps to cut after sorting
 #define DIFFICULTY_BLOCKS_COUNT                         DIFFICULTY_WINDOW + DIFFICULTY_LAG
 
+#define DIFFICULTY_WINDOW_V7                            60
+#define DIFFICULTY_BLOCKS_COUNT_V7                      DIFFICULTY_WINDOW_V7 + 1
 
 #define CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V1   DIFFICULTY_TARGET_V1 * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS
 #define CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V2   DIFFICULTY_TARGET_V2 * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS
